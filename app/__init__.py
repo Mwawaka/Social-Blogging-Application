957e91d812f48f92055b37181bb85052ce575fa0
Mwawaka <kedskeds90@gmail.com>
2022-06-04 18:49:35 +0300

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from math import sqrt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-
+from flask_moment import Moment
 
 db=SQLAlchemy()
+mail=Mail()
+moment=Moment()
