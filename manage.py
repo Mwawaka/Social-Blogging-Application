a575dd7cd2340ff8bf569dce481f49ac05e6505a
Mwawaka <kedskeds90@gmail.com>
2022-06-04 19:54:46 +0300

    manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,17 @@
+from multiprocessing import managers
 import os
 from app import db,create_app
 from flask_script import Shell,Manager
+
+
+app=create_app(os.environ.get('FLASK_CONFIG') or 'default')
+manager=Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app,db=db)
+manager.add_command('shell',Shell(make_context=make_shell_context))
+
+
+if __name__=='main':
+    manager.run()
