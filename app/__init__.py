d3f2ff903da41aaaec862cea9fdf33873845a2f2
Mwawaka <kedskeds90@gmail.com>
2022-06-04 18:59:25 +0300

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from distutils.command.config import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
@@ -7,3 +7,18 @@ from flask_moment import Moment
 db=SQLAlchemy()
 mail=Mail()
 moment=Moment()
+
+# Factory Function
+
+def create_app(config_name):
+    app=Flask(__name__)
+    
+    app.config.from_object(config[config_name]) #from_object() method ensures that the configurations in the config.py can be imported directly into the application
+    
+    config[config_name].init_app(app) #.init_app() ensures complete initializtion of the extensions declared above 
+    
+    db.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    
+    return app
