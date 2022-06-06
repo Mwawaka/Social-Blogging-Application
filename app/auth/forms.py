cea0ff2b1ecb1082a39c87a7271dba9a8dffd78b
Mwawaka <kedskeds90@gmail.com>
2022-06-06 20:55:40 +0300

    Commit

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,4 @@
 from email import message
-from tkinter import Label
 from flask_wtf import  FlaskForm
 from wtforms import StringField,PasswordField,EmailField,BooleanField,SubmitField
 from wtforms.validators import DataRequired,Length,EqualTo,Email
