0dec281d924f17d15c5054b717ec37c4b1f7c3ee
Mwawaka <kedskeds90@gmail.com>
2022-06-06 12:30:08 +0300

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,26 @@ from flask_mail import Mail
 from flask_moment import Moment
 from config import config
 
-db=SQLAlchemy()
-mail=Mail()
-moment=Moment()
+db = SQLAlchemy()
+mail = Mail()
+moment = Moment()
 
 
 # Factory Function
 
 def create_app(config_name):
-    app=Flask(__name__)
-    
-    app.config.from_object(config[config_name]) #from_object() method ensures that the configurations in the config.py can be imported directly into the application
-    
-    # config[config_name].init_app(app) #.init_app() ensures complete initializtion of the extensions declared above 
-    
+    app = Flask(__name__)
+
+    # from_object() method ensures that the configurations in the config.py can be imported directly into the application
+    app.config.from_object(config[config_name])
+
+    # config[config_name].init_app(app) #.init_app() ensures complete initializtion of the extensions declared above
+
     db.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
-    
-    
-    #Registering the main blueprint
+
+    # Registering the main blueprint
     from app.main import main as main_bp
     app.register_blueprint(main_bp)
     return app
