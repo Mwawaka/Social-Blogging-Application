28db0edb78d6faadf8673a0c95dfb413e675db52
Mwawaka <kedskeds90@gmail.com>
2022-06-06 20:55:47 +0300

    Commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from app.auth import auth
-from auth.forms import RegistrationForm
+from .forms import RegistrationForm
 
 @auth.route('/register',methods=['GET','POST'])
 def register():
     form=RegistrationForm()
-    return render_template('auth/registration.html')
+    return render_template('auth/registration.html',form=form)
