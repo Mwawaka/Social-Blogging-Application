7253284c9d4a634e021d3f2d8d2fedfaad736867
Mwawaka <kedskeds90@gmail.com>
2022-06-06 20:46:02 +0300

    Commit

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+from email import message
+from tkinter import Label
+from flask_wtf import  FlaskForm
+from wtforms import StringField,PasswordField,EmailField,BooleanField,SubmitField
+from wtforms.validators import DataRequired,Length,EqualTo,Email
+
+class RegistrationForm(FlaskForm):
+    username=StringField(label='Username :',validators=[DataRequired(),Length(min=6,max=30)])
+    email=EmailField(label='Email :',validators=[DataRequired(),Email()])
+    password1=PasswordField(label='Password :')
+    password2=PasswordField(label='Confirm Password',validators=[DataRequired(),EqualTo(password1)],message='Password Must Match!')
+    submit=SubmitField(label='Register')
