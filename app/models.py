2d408d065ebd8efddbb71e2a3840bfbbc132be88
Mwawaka <kedskeds90@gmail.com>
2022-06-06 14:55:53 +0300

    Commit

--- app/models.py
+++ app/models.py
@@ -1,25 +1,25 @@
-import email
-from unicodedata import name
 from app import db
 
 
 class User(db.Model):
-    __tablename__='users'
-    id=db.Column(db.Integer(),primary_key=True)
-    username=db.Column(db.String(30),nullable=False,unique=True,index=True)
-    email=db.Column(db.String(60),nullable=False,unique=True,index=True)
-    password_hash=db.Column(db.String(128),nullable=False)
-    role_id=db.Column(db.Integer(),db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(30), nullable=False,
+                         unique=True, index=True)
+    email = db.Column(db.String(60), nullable=False, unique=True, index=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
 
+
 class Role(db.Model):
-    __tablename__='roles'
-    id=db.Column(db.Integer(),primary_key=True)
-    name=db.Column(db.String(30),nullable=False,unique=True)
-    users=db.relationship('User',backref='role',lazy=True)
-    
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    users = db.relationship('User', backref='role', lazy=True)
+
     def __repr__(self):
         return f'Role {self.name}'
-#index-ensures that queries are more efficient
+# index-ensures that queries are more efficient
