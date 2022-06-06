76774764646cb37fc886cc571acf7d0f5fee606a
Mwawaka <kedskeds90@gmail.com>
2022-06-06 13:54:03 +0300

    Commit

--- flasky.py
+++ flasky.py
@@ -1,11 +1,13 @@
 import os
 from app import db,create_app
+from app.models import User,Role
+
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db)
+    return dict(db=db,User=User,Role=Role)
 
 if __name__=='__main__':
     app.run()
