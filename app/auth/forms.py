d66a1416ffef07aaf3cb66cb72558ca3befe5f1a
Mwawaka <kedskeds90@gmail.com>
2022-06-07 05:17:58 +0300

    Commit

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,24 @@
-from flask_wtf import  FlaskForm
-from wtforms import StringField,PasswordField,EmailField,BooleanField,SubmitField
-from wtforms.validators import DataRequired,Length,EqualTo,Email
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
 
 class RegistrationForm(FlaskForm):
-    username=StringField(label='Username :',validators=[DataRequired(),Length(min=6,max=30)])
-    email=EmailField(label='Email Address:',validators=[DataRequired(),Email()])
-    password1=PasswordField(label='Password :')
-    password2=PasswordField(label=' Password :',validators=[DataRequired(),EqualTo(password1)])
-    submit=SubmitField(label='Register')
-    
+    username = StringField(label='Username :', validators=[
+                           DataRequired(), Length(min=6, max=30)])
+    email = EmailField(label='Email Address:', validators=[
+                       DataRequired(), Email()])
+    password1 = PasswordField(label='Password :', validators=[
+                              DataRequired(), Length(min=6)])
+    password2 = PasswordField(label=' Password :', validators=[
+                              DataRequired(), EqualTo(password1)])
+    submit = SubmitField(label='Create Account')
+
+
 class LoginForm(FlaskForm):
-    email=EmailField(label='Email Address :',validators=[DataRequired(),Email()])
-    login_password=PasswordField(label='Password :',validators=[DataRequired()])
-    submit=SubmitField(label='Login')
+    email = EmailField(label='Email Address :', validators=[
+                       DataRequired(), Email()])
+    login_password = PasswordField(
+        label='Password :', validators=[DataRequired()])
+    remember_me = BooleanField(label='Keep me signed in')
+    submit = SubmitField(label='Sign In')
