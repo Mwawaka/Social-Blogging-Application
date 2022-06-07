8e2bbef0ef3432013aace85490cc811dc9bdb9fe
Mwawaka <kedskeds90@gmail.com>
2022-06-07 05:31:29 +0300

    Commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,17 +6,21 @@ from app.models import User
 from app import db
 
 
-
-@auth.route('/register',methods=['GET','POST'])
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form=RegistrationForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
-        new_user=User(username=form.username.data,email=form.email.data,password_hash=form.password1.data)
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password1.data
+        )
         db.session.add(new_user)
         db.session.commit()
-    return render_template('auth/register.html',form=form)
+    return render_template('auth/register.html', form=form)
 
-@auth.route('/login',methods=['GET','POST'])
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form=LoginForm()
-    return render_template('auth/login.html',login_form=login_form)
+    login_form = LoginForm()
+    return render_template('auth/login.html', login_form=login_form)
