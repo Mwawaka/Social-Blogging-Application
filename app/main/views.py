82be37d5a75f619cc103955f50cf6d35238b34da
Mwawaka <kedskeds90@gmail.com>
2022-06-07 10:32:17 +0300

    Commit

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required
 from app.main import main
 from app import db
 
-@main.route('/',methods=['GET','POST'])
+@main.route('/home',methods=['GET','POST'])
 @login_required
 def index():
     return render_template('home.html')
