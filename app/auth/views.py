061e2f1205f6e8f91f2995f2111c5eb3235e3da5
Mwawaka <kedskeds90@gmail.com>
2022-06-07 10:08:48 +0300

    Commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
 from cmath import log
 from flask import flash, redirect, render_template, url_for
 from flask_login import login_user
+from requests import request
 from app.auth import auth
 from .forms import LoginForm, RegistrationForm
 from app.models import User
@@ -24,7 +25,7 @@ def register():
         for err_msg in form.errors.values():
             flash(
                 f'There was an error in creating an account: {err_msg}', category='danger')
-            
+
     return render_template('auth.register.html', form=form)
 
 
@@ -32,11 +33,18 @@ def register():
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user=User.query.filter_by(
+        user = User.query.filter_by(
             email=login_form.email.data
         ).first()
         if user and user.verify_password(login_password=login_form.login_password.data):
-            login_user(user)
-            flash(f'You have successfull signed in as :{user.username}')
-        
+            login_user(user, login_form.remember_me)
+            next = request.args.get('next')
+            # the original url protected from unauthorized access is stored in next query string
+            if next is None or not next.startswith('/'):
+                # The URL in next is validated to make sure it is a relative URL
+                next = url_for('main.index')
+                # if next query string is not available user is then redirected to the home page
+            return redirect(next)
+        flash(f'You have successfull signed in as :{user.username}')
+
     return render_template('auth/login.html', login_form=login_form)
