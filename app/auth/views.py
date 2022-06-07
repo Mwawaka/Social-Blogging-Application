5ee8c6025c122472c8710996bc74fbd25ecd0bb0
Mwawaka <kedskeds90@gmail.com>
2022-06-07 05:46:16 +0300

    Commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 import email
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 from app.auth import auth
 from .forms import LoginForm, RegistrationForm
 from app.models import User
@@ -17,6 +17,11 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+        flash('Successfully registered.You can now Sign In!',category='info')
+        redirect(url_for('auth/login'))
+    if form.errors !={}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error in creating an account: {err_msg}',category='danger')
     return render_template('auth/register.html', form=form)
 
 
