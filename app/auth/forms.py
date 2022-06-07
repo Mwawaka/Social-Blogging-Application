8e48959155b8ff9ab22b55d6b253863aad653df3
Mwawaka <kedskeds90@gmail.com>
2022-06-07 04:11:15 +0300

    Commit

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired,Length,EqualTo,Email
 
 class RegistrationForm(FlaskForm):
     username=StringField(label='Username :',validators=[DataRequired(),Length(min=6,max=30)])
-    email=EmailField(label='Email :',validators=[DataRequired(),Email()])
+    email=EmailField(label='Email Address:',validators=[DataRequired(),Email()])
     password1=PasswordField(label='Password :')
-    password2=PasswordField(label='Confirm Password',validators=[DataRequired(),EqualTo(password1)],message='Password Must Match!')
+    password2=PasswordField(label=' Password :',validators=[DataRequired(),EqualTo(password1)])
     submit=SubmitField(label='Register')
