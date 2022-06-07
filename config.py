833aee1768fd5d520289e1e11add406bbf6be1a6
Mwawaka <kedskeds90@gmail.com>
2022-06-07 15:53:02 +0300

    Commit

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     MAIL_USE_SSL=False
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX=['Flasky']
-    FLASKY_MAIL_SENDER=''
+    FLASKY_MAIL_SUBJECT_PREFIX=['Blogvolution']
+    FLASKY_MAIL_SENDER=os.environ.get('SENDER_MAIL')
     FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     
