31f95408e9bdc7d6851f2595a416926c70cf6827
Mwawaka <kedskeds90@gmail.com>
2022-06-07 10:13:39 +0300

    Commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
-from crypt import methods
 from datetime import datetime
-
 from flask import render_template
+from flask_login import login_required
 from app.main import main
 from app import db
 
 @main.route('/',methods=['GET','POST'])
+@login_required
 def index():
-    return render_template('base.html')
+    return render_template('home.html')
