465fe05a4eb3399ccd8c0e9b7c1469e3d803ff85
Mwawaka <kedskeds90@gmail.com>
2022-06-08 05:50:30 +0300

    Commit

--- app/emails.py
+++ app/emails.py
@@ -1,13 +1,15 @@
 from flask_mailing import Message
-from flask import current_app, render_template
+from flask import current_app, jsonify, render_template
 from app import mail
 
 
         
-def send_email(to,subject,template,**kwargs):
+async def send_email(to,subject,template,**kwargs):
     app=current_app._get_current_object()
     msg=Message(str(app.config['FLASKY_MAIL_SUBJECT_PREFIX'])+' '+subject,sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
     msg.body=render_template(template+'.txt',**kwargs)
     msg.html=render_template(template +'.html',**kwargs)
-    mail.send(msg)
+    
+    await mail.send_message(msg)
+    return jsonify(status_code=200,content={"message":"Email has been sent."})
     
