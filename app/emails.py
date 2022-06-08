bfb6ecd34639e6ef9879b298d122ad990c36952f
Mwawaka <kedskeds90@gmail.com>
2022-06-08 09:21:01 +0300

    Commit

--- app/emails.py
+++ app/emails.py
@@ -6,7 +6,7 @@ from app import mail
         
 async def send_email(to,subject,template,**kwargs):
     app=current_app._get_current_object()
-    msg=Message(str(app.config['FLASKY_MAIL_SUBJECT_PREFIX'])+' '+subject,sender=app.config['MAIL_FROM'],recipients=[to])
+    msg=Message(str(app.config['FLASKY_MAIL_SUBJECT_PREFIX'])+' '+subject,sender=app.config['MAIL_DEFAULT_SENDER'],recipients=[to])
     msg.body=render_template(template+'.txt',**kwargs)
     msg.html=render_template(template +'.html',**kwargs)
     
