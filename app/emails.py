b44efc0dbfc739717d3a07092d8233cd92a59245
Mwawaka <kedskeds90@gmail.com>
2022-06-08 10:43:24 +0300

    Commit

--- app/emails.py
+++ app/emails.py
@@ -5,7 +5,7 @@ from app import mail
 def send_email(to, subject,template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(
-        subject=str(app.config['FLASKY_MAIL_SUBJECT_PREFIX'])+' '+subject,
+        str(app.config['FLASKY_MAIL_SUBJECT_PREFIX'])+' '+subject,
         sender=app.config['MAIL_DEFAULT_SENDER'],
         recipients=[to]
      )
