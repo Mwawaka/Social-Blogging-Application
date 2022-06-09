9b854c80e0e638573f67f88b7989910010786b78
Mwawaka <kedskeds90@gmail.com>
2022-06-09 12:35:55 +0300

    Commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from datetime import datetime
 from flask import render_template
 from flask_login import login_required
 from app.main import main
 
 @main.route('/')
+@login_required
 def landing():
     return render_template('landing.html')
 
