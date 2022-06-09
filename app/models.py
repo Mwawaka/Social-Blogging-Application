401787941bf8e564038ea6b0320961868e38e77c
Mwawaka <kedskeds90@gmail.com>
2022-06-09 21:27:32 +0300

    Commit

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from flask import current_app
-from app import db,bcrypt
+from app import db, bcrypt
 from flask_login import UserMixin
 from app import login_manager
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from itsdangerous import URLSafeTimedSerializer as Serializer
 
 
@@ -12,42 +12,45 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-
-class User(db.Model,UserMixin):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False,
                          unique=True, index=True)
-    email = db.Column(db.String(length=60), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(length=60), nullable=False,
+                      unique=True, index=True)
     password_hash = db.Column(db.String(length=128), nullable=False)
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id'))
-    confirmed=db.Column(db.Boolean(),nullable=False,default=False)
-    
-    #Password encryption
+    confirmed = db.Column(db.Boolean(), nullable=False, default=False)
+
+    # Password encryption
     @property
     def password(self):
-        #Prevent reading password
+        # Prevent reading password
         raise AttributeError('Password is not readable')
         # return self.password
-    
+
     @password.setter
-    def password(self,plain_text_password):
-        self.password_hash=bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
-        
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(
+            plain_text_password).decode('utf-8')
+
     # Password verification
-    def verify_password(self,login_password):
-        return bcrypt.check_password_hash(self.password_hash,login_password)
+    def verify_password(self, login_password):
+        return bcrypt.check_password_hash(self.password_hash, login_password)
 
     # Token authentication using URLSafeTimedSerializer
-    #Generates the token
+    # Generates the token
+
     def generate_confirmation_token(self):
-        s=Serializer(current_app.config['SECRET_KEY'])
-        return s.dumps({'confirm':self.id},salt=current_app.config['SECURITY_PASSWORD_SALT'])
-    #Confirms the token
-    def confirm_token(token,expiration=3600):
-        s=Serializer(current_app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'confirm': self.id}, salt=current_app.config['SECURITY_PASSWORD_SALT'])
+
+    # Confirms the token
+    def confirm_token(token, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            data=s.loads(
+            data = s.loads(
                 token,
                 salt=current_app.config['SECURITY_PASSWORD_KEY'],
                 max_age=expiration
@@ -55,8 +58,7 @@ class User(db.Model,UserMixin):
         except:
             return False
         return data
-        
-       
+
     def __repr__(self):
         return f'User {self.username}'
 
