592a2ae1b6873c1ad4c8ee0e6a6f7a679f7d8eea
Mwawaka <kedskeds90@gmail.com>
2022-06-15 14:09:54 +0300

    Update:Password reset view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
+from crypt import methods
+from curses import reset_shell_mode
 from re import I
-from flask import  flash,  redirect, render_template, url_for, request
+from flask import flash,  redirect, render_template, url_for, request
 from flask_login import current_user, login_required, login_user, logout_user
 from app.auth import auth
-from .forms import ChangeEmail, ChangePassword, LoginForm, PasswordResetRequestForm, RegistrationForm,PasswordReset
+from .forms import ChangeEmail, ChangePassword, LoginForm, PasswordResetRequestForm, RegistrationForm, PasswordResetForm
 from app.models import User
 from app import db
 from app.emails import send_email
@@ -14,7 +16,6 @@ from app.emails import send_email
 def before_request():
     if current_user.is_authenticated and not current_user.confirmed and request.blueprint != 'auth' and request.endpoint != 'static':
         return redirect(url_for('auth.unconfirmed'))
-
 
 
 # Route that deals with users who have not confirmed their accounts
@@ -52,27 +53,24 @@ def register():
     return render_template('auth/register.html', form=form)
 
 
-
-#route to hanlde account confirmation
-@auth.route('/confirm/<token>',methods=['GET','POST'])
+# route to hanlde account confirmation
+@auth.route('/confirm/<token>', methods=['GET', 'POST'])
 @login_required
 def confirm(token):
     if current_user.confirmed:
-        flash('Account has already been confirmed',category='success')
+        flash('Account has already been confirmed', category='success')
         return redirect(url_for('main.landing'))
-    
-    user=User.confirm_token(token)
+
+    user = User.confirm_token(token)
     if user is None:
-         flash('The confirmation link is invalid or has already expired',category='info')
-         return redirect(url_for('auth.unconfirmed'))
-    user.confirmed=True 
+        flash('The confirmation link is invalid or has already expired',
+              category='info')
+        return redirect(url_for('auth.unconfirmed'))
+    user.confirmed = True
     db.session.add(user)
     db.session.commit()
-    flash('You have successfully confirmed your account',category='success')
-    return redirect(url_for('main.landing')) 
-   
-    
-    
+    flash('You have successfully confirmed your account', category='success')
+    return redirect(url_for('main.landing'))
 
 
 # Resending confirmation email
@@ -84,7 +82,6 @@ def resend_confirmation_email():
                'auth/email/confirm', new_user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email', category='info')
     return redirect(url_for('auth.unconfirmed'))
-
 
 
 # Login
@@ -149,27 +146,57 @@ def change_email_request():
     return render_template('auth/change_email.html', change_email=change_email)
 
 
-
 # route that handles the change email token
-@auth.route('/change_email/<token>',methods=['GET','POST'])
+@auth.route('/change_email/<token>', methods=['GET', 'POST'])
 def change_email(token):
-    email,user=User.confirm_email_token(token)
+    email, user = User.confirm_email_token(token)
     if user is None:
-        flash('The confirmation link is invalid or has expired',category='danger')
+        flash('The confirmation link is invalid or has expired', category='danger')
         return redirect(url_for('main.landing'))
-    user.email=email
+    user.email = email
     db.session.add(user)
     db.session.commit()
-    flash('Email has successfully been changed.',category='success')
+    flash('Email has successfully been changed.', category='success')
     return redirect(url_for('main.landing'))
 
-#Request for password reset
-@auth.route('/reset_password')
+
+# Request for password reset
+@auth.route('/reset_password', methods=['GET', 'POST'])
 def reset_password_request():
-  reset_form=PasswordResetRequestForm()
-  if reset_form.validate_on_submit():
-    pass
-  return render_template('auth/reset_password.html',reset_form=reset_form)    
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.landing'))
+    reset_form = PasswordResetRequestForm()
+    if reset_form.validate_on_submit():
+        user = User.query.filter_by(email=reset_form.email.data).first()
+        if user:
+            token = user.reset_password_token()
+            send_email(user.email, 'Reset your password',
+                       'auth/email/password_reset', user=user, token=token)
+            flash('An email containing a reset instructions has been send to your mailtrap address', category='success')
+            return redirect(url_for('auth.login'))
+        return render_template('auth/reset_password.html', reset_form=reset_form)
+
+# Route that handles reset password token
+@auth.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    user = User.confirm_reset_token(token)
+    if user is None:
+        flash('The confirmation link is invalid or has expired', category='danger')
+        return redirect(url_for('main.landing'))
+    if not user.is_anonymous:
+        return redirect(url_for('main.landing'))
+    reset = PasswordResetForm()
+    if reset.validate_on_submit():
+        user.password=reset.password1.data
+        db.session.add(user)
+        db.session.commit()
+        flash('Your password has been updated',category='success')
+        return redirect(url_for('auth.login'))
+    else:
+        flash('Invalid email or password',category='danger')
+    return render_template('auth/reset_password.html',reset=reset)        
+      
+
 # login out
 @auth.route('/logout')
 @login_required
