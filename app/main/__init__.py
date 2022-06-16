a9087da4a6cb18940c34f8d6ca32712392dc946e
Mwawaka <kedskeds90@gmail.com>
2022-06-16 05:40:21 +0300

    Update:Add app context processor

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 
+from app.models import Permission
+
 main=Blueprint('main',__name__)
 
 from app.main import error,views
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
