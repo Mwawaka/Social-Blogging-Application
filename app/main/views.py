26f684362be1b88f034f27c2a14c900cde427ba6
Mwawaka <kedskeds90@gmail.com>
2022-06-16 08:47:45 +0300

    Update:Add profile pages route

--- app/main/views.py
+++ app/main/views.py
@@ -3,11 +3,9 @@ from flask_login import login_required
 from app.main import main
 
 @main.route('/')
+@main.route('/home',methods=['GET','POST'])
 @login_required
 def landing():
     return render_template('landing.html')
 
-@main.route('/home',methods=['GET','POST'])
-@login_required
-def index():
-    return render_template('home.html')
+#profile pages route
