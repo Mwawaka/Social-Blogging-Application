ed18595f5d3f5823f5c448f7a7c6ccf6f387511f
Mwawaka <kedskeds90@gmail.com>
2022-06-20 15:15:16 +0300

    Commit

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,EmailField,BooleanField,SubmitField
+from wtforms import StringField,PasswordField,EmailField,BooleanField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired,Length,Email
 
 
 class NameForm(FlaskForm):
     name=StringField(label='What is your name ?',validators=[Length(min=6,max=30),DataRequired()])
     submit=SubmitField(label='Submit')
+class EditProfileForm(FlaskForm):
+    name=StringField(label='Real Name :',validators=[DataRequired(),Length(min=6,max=30)])
+    location=StringField(label='Location',validators=[DataRequired(),Length(min=6,max=60)])
+    about_me=TextAreaField(label='About me :')
+    submit=SubmitField(label='Edit Profile')
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
-from flask_login import login_required
+from flask import flash, render_template
+from flask_login import current_user, login_required
 from app.main import main
+from app.models import User
+from .forms import EditProfileForm
+from app import db
 
 @main.route('/')
 @main.route('/home',methods=['GET','POST'])
@@ -9,3 +12,24 @@ def landing():
     return render_template('landing.html')
 
 #profile pages route
+@main.route('/user/<username>')
+def user(username):
+    user=User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html',user=user)
+
+@main.route('/edit_profile',methods=['GET','POST'])
+@login_required
+def edit_profile():
+    edit_form=EditProfileForm()
+    if edit_form.validate_on_submit():
+        current_user.name=edit_form.name.data
+        current_user.location=edit_form.location.data
+        current_user.about_me=edit_form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('You have successfully edited your profile',category='success')
+        return render_template('.user',username=current_user.username)
+    edit_form.name.data=current_user.name
+    edit_form.location.data=current_user.location
+    edit_form.about_me.data=current_user.about_me
+    return render_template('edit_profile.html',edit_form=edit_form)
